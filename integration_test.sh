e5f2213454665ecc941349c11a90f97c3deb847a
nghazalibeiklar <nghazalibeiklar@atlassian.com>
2021-02-15 14:02:29 +1100

    DCKUBE-111 - Add the initial value for exit code in test script ans also check for making sure SNYK_TOKEN is defined before authentication

--- integration_test.sh
+++ integration_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TEST_RESULT=0
+
 check_for_failure() {
     exit_code=$1
     if [ $exit_code -ne 0 ]; then
@@ -7,10 +9,15 @@ check_for_failure() {
     fi
 }
 
+if [[ -z "${SNYK_TOKEN}" ]]; then
+    echo 'Security scan is interrupted because Snyk authentication token (SNYK_TOKEN) is not defined!.'
+    exit 1
+fi
+
 # snyk Authentication
 snyk auth $SNYK_TOKEN
 
-# perform security scan
+# perform security scan - ignoring low and medium vulnerabilities
 snyk container test $1 --severity-threshold=high
 # evaluate the result
 check_for_failure $?
