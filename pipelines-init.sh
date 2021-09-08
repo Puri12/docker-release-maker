3d3e238a978c2639dba023a89a4bdc109812c0b2
Steve Smith <ssmith@atlassian.com>
2021-09-08 11:12:39 +1000

    DCD-1323: Typo in init helper.

--- pipelines-init.sh
+++ pipelines-init.sh
@@ -8,5 +8,5 @@ pip install -q -r ${SHDIR}/requirements.txt
 
 HADOLINT_VERSION=2.1.0
 HADOLINT_URL=https://github.com/hadolint/hadolint/releases/download/v${HADOLINT_VERSION}/hadolint-Linux-x86_64
-url -sL ${HADOLINT_URL} -o /usr/local/bin/hadolint
+curl -sL ${HADOLINT_URL} -o /usr/local/bin/hadolint
 chmod +x /usr/local/bin/hadolint
